adf044bd929efaeef582d9329a419246cd970584
kevinjerke <kevinjerke@10.30.21.25>
2016-08-30 16:16:59 -0500

    Revert "Added Animal class and modified main to include another message "
    This reverts commit 61fc1dac21d586bf1990ccb14bf4b1d851376274.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -17,8 +17,7 @@ public class FirstGit {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hello Java");
-        System.out.println("Hello Class");
+        System.out.println("Hello World");
     }
     
 }
